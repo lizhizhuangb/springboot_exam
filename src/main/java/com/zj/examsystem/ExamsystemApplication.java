b12fc9025018ca960fc6e551cf0b31eda1ccd411
lanni <yutu95@126.com>
2024-03-15 17:03:07 +0800

    springcloud版本

--- src/main/java/com/zj/examsystem/ExamsystemApplication.java
+++ src/main/java/com/zj/examsystem/ExamsystemApplication.java
@@ -3,9 +3,12 @@ package com.zj.examsystem;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @MapperScan("com.zj.examsystem.mapper")
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ExamsystemApplication {
     public static void main(String[] args) {
         //        String filePath = new File("").getAbsolutePath() + "\\src\\main\\resources\\python\\textSimilarity.py";
